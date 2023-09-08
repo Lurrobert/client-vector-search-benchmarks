42fcd7d0d9bd12a3e81bce8b6d60e2e1239c7ae6
robertlukoshko <robert.lukoshko@gmail.com>
2023-09-09 01:29:45 +0200

    inti

---
+++ src/app/page.js
@@ -0,0 +1,211 @@
+"use client"
+
+import { useState, useEffect, useRef, useCallback } from 'react'
+
+export default function Home() {
+
+  const [result, setResult] = useState(null);
+  const [ready, setReady] = useState(null);
+  const [inputValue, setInputValue] = useState(250);
+  const [inProgress, setInProgress] = useState(false);
+  const [currentProgress, setCurrentProgress] = useState(0);
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
+  const [size, setSize] = useState(null);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const worker = useRef(null);
+  useEffect(() => {
+    if (!worker.current) {
+      worker.current = new Worker(new URL('./worker.js', import.meta.url), {
+        type: 'module'
+      });
+    }
+
+    const onMessageReceived = (e) => {
+      switch (e.data.type) {
+        case 'classify':
+          switch (e.data.status) {
+            case 'initiate':
+              setReady(false);
+              break;
+            case 'complete':
+              setReady(true);
+              setResult(e.data.output)
+              setInProgress(false);
+              setCurrentProgress(0)
+              break;
+            case 'update':
+              setReady(false);
+              setCurrentProgress(e.data.progress)
+              break;
+            case 'error':
+              setReady(true);
+              setResult(e.data.error)
+              setInProgress(false);
+              break;
+          }
+          break;
+        case 'search':
+          switch (e.data.status) {
+            case 'initiate':
+              setReady(false);
+              break;
+            case 'complete':
+              setReady(true);
+              setSearchResult(e.data.output)
+              setInProgress(false);
+              break;
+            case 'update':
+              setReady(false);
+              setCurrentProgress(e.data.progress)
+              break;
+            case 'error':
+              setReady(true);
+              setResult(e.data.error)
+              setInProgress(false);
+              break;
+            case 'size':
+              setReady(true);
+              setSize(e.data.output)
+              setInProgress(false);
+              break;
+          }
+          break;
+      }
+    };
+
+    worker.current.addEventListener('message', onMessageReceived);
+    return () => worker.current.removeEventListener('message', onMessageReceived);
+  });
+
+  const classify = useCallback((text) => {
+    if (worker.current) {
+      worker.current.postMessage({ type: 'classify', text });
+    }
+  }, []);
+
+  const searchEmbeddings = useCallback((text) => {
+    if (worker.current) {
+      worker.current.postMessage({ type: 'search', text });
+    }
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-12">
+      <h1 className="text-5xl font-bold mb-2 text-center">Transformers.js</h1>
+      <h2 className="text-2xl mb-4 text-center">Next.js template (client-side)</h2>
+      <input
+        type="range"
+        className="w-full max-w-xs p-2 border border-gray-300 rounded mb-4"
+        placeholder="Enter number of vectors to generate"
+        min="100"
+        max="5000"
+        step="250"
+        defaultValue="250"
+        onChange={handleInputChange}
+      />
+      {/* manual input */}
+      <input
+        type="number"
+        className="w-full max-w-xs p-2 border border-gray-300 rounded mb-4"
+        placeholder="Enter number of vectors to generate"
+        min="100"
+        max="60000"
+        defaultValue="250"
+        onChange={handleInputChange}
+      />
+
+      <p className="text-center mb-4">Number of vectors to generate: <span className="font-bold">{inputValue}</span></p>
+      <button
+        className="w-full max-w-xs p-2 border border-gray-300 rounded mb-4"
+        onClick={e => {
+          setInProgress(true);
+          classify(parseInt(inputValue))
+        }}
+      >
+        Submit
+      </button>
+
+      {/* panels */}
+      <div className="flex min-h-screen">
+        {/* Left Panel */}
+        <div className="w-1/2 flex flex-col items-center justify-center p-12">
+          <h1 className="text-5xl font-bold mb-2 text-center">Client side compute</h1>
+          {ready !== null && (
+            <div>
+              {
+                inProgress ? (
+                  <>
+                    <p>Loading...</p>
+                    <div className="relative pt-3">
+                      <div className="overflow-hidden h-4 mb-4 text-sm flex rounded bg-blue-200">
+                      <div style={{ width: `${(currentProgress/inputValue)*100}%`, height: '70px' }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+                      <div>{currentProgress} / {inputValue} </div>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <h1 className="text-5xl font-bold mb-2 text-center">
+                    <pre>{result} seconds</pre>
+                    <pre>for {inputValue} vectors</pre>
+                  </h1>
+                  
+                )
+              }
+            </div>
+          )}
+        </div>
+        <div className="w-1/2 flex flex-col items-center justify-center p-12">
+          <h1 className="text-5xl font-bold mb-2 text-center">Embedding Search</h1>
+          <div className="w-full max-w-xs p-2 border border-gray-300 rounded mb-4">
+            <input
+              type="text"
+              className="w-full p-2 border border-gray-300 rounded"
+              placeholder="Search embeddings..."
+              onChange= {e => {
+                setSearchValue(e.target.value)
+              }}
+
+            />
+          </div>
+          <button
+            className="w-full max-w-xs p-2 border border-gray-300 rounded"
+            onClick={e => {
+              setInProgress(true);
+              searchEmbeddings(searchValue)
+            }}
+          >
+            Search
+          </button>
+          {ready !== null && (
+            <div>
+              {
+                inProgress ? (
+                  <>
+                    <p>Loading...</p>
+                  </>
+                ) : (
+                  <>
+                    {searchResult && searchResult.map((item, index) => (
+                      <div key={index}>
+                        <div className="w-full max-w-xs p-2">
+                          <pre>{`Similarity: ${item.similarity}`}</pre>
+                          <pre>{`Match: ${JSON.stringify(item.object.name)}`}</pre>
+                        </div>
+                      </div>
+                    ))}
+                  </>
+                )
+              }
+              TOTAL SIZE: {size}
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
+  )
+}
---
+++ src/app/worker.js
@@ -0,0 +1,124 @@
+import { getEmbedding, EmbeddingIndex } from '@robertoooooo/client-vector-search'
+import axios from 'axios';
+
+
+export async function getServerSideProps() {
+    let vocabulary = [];
+  
+    try {
+        console.log('Downloading the file...');
+        const response = await axios.get('https://raw.githubusercontent.com/gokhanyavas/Oxford-3000-Word-List/master/Oxford%203000%20Word%20List.txt');
+      const data = response.data;
+  
+      vocabulary = data.split('\n');
+    } catch (error) {
+      console.error('Error downloading the file:', error);
+    }
+  
+    return vocabulary
+  }
+  
+const vocabulary =  await getServerSideProps();
+
+const index = new EmbeddingIndex(); 
+
+async function generateEmbeddings(numVectors, numWords = 5) {
+    const vocabLength = vocabulary.length;
+    let startTime, endTime;
+    self.postMessage({
+        type: 'classify',
+        status: 'initiate',
+    });
+
+    startTime = new Date();
+    for (let i = 0; i < numVectors; i++) {
+        var randomWords = ''
+        for (let j = 0; j < numWords; j++) {
+            const randomIndex = Math.floor(Math.random() * (vocabLength-1));
+            randomWords += vocabulary[randomIndex] + ' ';
+        }
+
+        const objectToAdd = { id: i, name: randomWords, embedding: await getEmbedding(randomWords) };
+        index.add(objectToAdd);
+
+        if ((i + 1) % 50 === 0) {
+            console.log('50v done...');
+            self.postMessage({
+                type: 'classify',
+                status: 'update',
+                progress: i + 1,
+            });
+        }
+    }
+    await index.saveIndexToDB("dbName", "ObjectStoreName");
+
+    endTime = new Date();
+    let timeDiff = (endTime - startTime) / 1000;
+    timeDiff = timeDiff.toFixed(2);
+    console.log(
+        'TT TIME', timeDiff
+    )
+    return timeDiff
+}
+
+// Listen for messages from the main thread
+self.addEventListener('message', async (event) => {
+    const { type, text } = event.data;
+
+    switch (type) {
+        case 'classify':
+            try {
+                // number of embeddings to generate
+                const generationTimes = await generateEmbeddings(text)
+                self.postMessage({
+                    type: 'classify',
+                    status: 'complete',
+                    output: generationTimes,
+                });
+            } catch (error) {
+                console.error(error);
+                self.postMessage({
+                    type: 'classify',
+                    status: 'error',
+                    error: error.message,
+                });
+            }
+            break;
+        case 'search':
+            try {
+                self.postMessage({
+                    status: 'initiate',
+                });            
+                const results = await index.search(
+                    await getEmbedding(text),
+                    { topK: 5 },
+                    true,
+                    'dbName',
+                    'ObjectStoreName'
+                )
+                console.log('results', results)
+                self.postMessage({
+                    type: 'search',
+                    status: 'complete',
+                    output: results,
+                });
+
+                const DBsize = await index.getAllObjectsFromDB('dbName', 'ObjectStoreName')
+                console.log('DBsize', DBsize.length)
+                self.postMessage({
+                    type: 'search',
+                    status: 'size',
+                    output: DBsize.length,
+                });
+            }
+            catch (error) {
+                console.error(error);
+                self.postMessage({
+                    type: 'search',
+                    status: 'error',
+                    error: error.message,
+                });
+            }
+            break;
+    }
+});
